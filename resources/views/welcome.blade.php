ed1c9f4c022fc1b350e0657b6122664b6b520ee1
Mohamed El Kawakibi <mohamed@justbetter.nl>
2017-07-07 15:45:38 +0200

    view update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel test omgeving
                 </div>
 
                 <div class="links">
